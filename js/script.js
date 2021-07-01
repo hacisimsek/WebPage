198e5adc5abd9120e58b313986b862b5e81fde2c
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2021-07-01 22:21:00 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// responsive nav menu
+
+const navMenu = () => {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navList = document.querySelector(".nav-list");
+
+  navbarToggler.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    navbarToggler.classList.toggle("toggle");
+  });
+};
+navMenu();
+
+// services slider
+
+const serviceChange = () => {
+  let firstChild, lastChild;
+  const arrowLeft = document.querySelector("#arrow-left");
+  const arrowRight = document.querySelector("#arrow-right");
+  const servicesAll = document.querySelector("#services-slider");
+
+  document.addEventListener("click", () => {
+    if (event.target === arrowLeft) {
+      lastChild = servicesAll.lastElementChild;
+      servicesAll.insertAdjacentElement("afterbegin", lastChild);
+    } else if (event.target === arrowRight) {
+      firstChild = servicesAll.firstElementChild;
+      servicesAll.insertAdjacentElement("beforeend", firstChild);
+    }
+  });
+};
+serviceChange();
+
+// ScrollReveal
+ScrollReveal().reveal(".ani-left", {
+  origin: "left",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
+
+ScrollReveal().reveal(".ani-right", {
+  origin: "right",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
+
+ScrollReveal().reveal(".ani-fade-up", {
+  opacity: 0,
+  easing: "ease-in",
+});
+
+ScrollReveal().reveal(".ani-bottom", {
+  origin: "bottom",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
+
+ScrollReveal().reveal(".ani-delay", {
+  delay: 250,
+});
+ScrollReveal().reveal(".ani-delay-2", {
+  delay: 500,
+});
+ScrollReveal().reveal(".ani-delay-3", {
+  delay: 750,
+});
